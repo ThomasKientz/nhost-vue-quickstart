14d5037cbb6b83dbce8034a8ee57c68a4e4c5bb3
Thomas Kientz <thomas@kientz.fr>
2024-03-07 20:49:37 +0100

    setup nhost

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { NhostClient } from '@nhost/vue'
+
+const nhost = new NhostClient({
+  subdomain: 'bqdvuskfcxsmjqgyxuvq',
+  region: 'eu-central-1'
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(nhost)
 
 app.mount('#app')
---
+++ src/nhost.ts
@@ -0,0 +1,6 @@
+import { NhostClient } from '@nhost/vue'
+
+export const nhost = new NhostClient({
+  subdomain: 'bqdvuskfcxsmjqgyxuvq',
+  region: 'eu-central-1'
+})
